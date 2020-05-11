42ccf349eddcc61b334bea2daf5d157cf2f4de5c
andresduarter22 <andresduarter13@gmail.com>
2020-05-11 03:57:29 +0000

    4

--- deploy.sh
+++ deploy.sh
@@ -59,7 +59,7 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --template-file $CF_FILE \
   --parameter-overrides Project=cf_lab2  \
-  --stack-name "my-awesome-stack5" \
+  --stack-name "my-awesome-stack6" \
   --capabilities CAPABILITY_NAMED_IAM
 fi
 
